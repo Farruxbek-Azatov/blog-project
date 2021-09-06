074e929d16df65940da884f0bd45b3f46fc27b0b
Farruxbek-Azatov <farruxbekazatov@gmail.com>
2021-09-06 22:59:10 +0300

    blog.urls, templates, static added and admin.py first written

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+from .models import Post
+
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(request, 'blog/post/list.html', {'posts': posts})
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, status='published', id=post_id)
+    return render(request, 'blog/post/detail.html', {'post': post})
