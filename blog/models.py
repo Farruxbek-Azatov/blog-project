440cb89574f9310d3bdde30cd8bfcebac39f1b60
Farruxbek-Azatov <farruxbekazatov@gmail.com>
2021-09-06 22:27:00 +0300

    Post model is changed, status field added

--- blog/models.py
+++ blog/models.py
@@ -2,18 +2,32 @@ from django.db import models
 
 
 class PublishedManager(models.Manager):
-    def get_queryset(self, *args, **kwargs):
-        return super().get_queryset(*args, **kwargs).filter(publish=True)
+    def get_queryset(self):
+        return super().get_queryset(PublishedManager, self).\
+            filter(status='published')
 
 
 class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published')
+    )
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(unique_for_date='publish')
     author = models.ForeignKey(User, related_name='posts',
                                on_delete=models.CASCADE)
-    title = models.CharField(max_length=250)
-    slug = models.SlugField(unique_for_date='created')
     body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now())
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    publish = models.BooleanField(default=False)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES,
+                              default='draft')
     objects = models.Manager()
     published = PublishedManager()
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
