1ac09d6b12a8d934b056c01cda6e40d2111e63d5
Farruxbek-Azatov <farruxbekazatov@gmail.com>
2021-09-13 12:11:52 +0300

    Some changes

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'blog'
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('post/<slug:post_slug>/', views.post_detail, name='post_detail'),
+    path('post/<int:year>/<int:month>/<int:day>/<slug:slug>/',
+         views.post_detail, name='post_detail'),
+    path('post/<int:post_id>/share/', views.post_share, name='post_share'),
 ]
